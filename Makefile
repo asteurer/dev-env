5f5918ae9eee2bde5f0a89e07efd3de1effb2dd1
Andrew Steurer <andrew.steurer@fermyon.com>
2025-02-10 23:16:07 -0600

    saving work
    Signed-off-by: Andrew Steurer <andrew.steurer@fermyon.com>

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ host := 192.168.5.155
 user := asteurer
 
 setup:
-	@ssh $(user)@$(host) 'bash -s' < setup.sh
+	@ssh $(user)@$(host) \
+		"OP_SERVICE_ACCOUNT_TOKEN='$$(op item get dev_env_op_service_account_token --vault DEV)' bash -s" < setup.sh
 
 ssh:
 	@ssh $(user)@$(host)
--- setup.sh
+++ setup.sh
@@ -4,8 +4,6 @@
 ###        Configure Access         ###
 #######################################
 
-echo "### Configure Access ###"
-
 # Remove the ability to log in with a password or as root
 sudo sed -i \
     -e 's/#PasswordAuthentication yes/PasswordAuthentication no/g' \
@@ -13,45 +11,49 @@ sudo sed -i \
     /etc/ssh/sshd_config
 
 #######################################
-###        Update Packages          ###
+###   Update and Install Packages   ###
 #######################################
 
 echo "### Update Packages ###"
 sudo dnf update
+sudo dnf install -y \
+    git \
+    zsh \
+    zsh-autosuggestions \
+    zsh-syntax-highlighting \
+    golang \
+    tmux \
+    neovim
+
 
 #######################################
-###          Install git            ###
+###      Install 1Password CLI      ###
+#######################################
+sudo dnf install -y unzip
+ARCH="amd64"; \
+OP_VERSION="v$(curl https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/N -s | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')"; \
+    curl -sSfo op.zip \
+    https://cache.agilebits.com/dist/1P/op2/pkg/"$OP_VERSION"/op_linux_"$ARCH"_"$OP_VERSION".zip \
+    && sudo unzip -od /usr/local/bin/ op.zip \
+    && rm op.zip
+
+
+#######################################
+###       Install dev-env Repo      ###
 #######################################
 
-echo "### Install git ###"
-sudo dnf install -y git
-
-#######################################
-###          Install repo           ###
-#######################################
-
-echo "### Install repo ###"
 git clone https://github.com/asteurer/dev-env
 
 #######################################
-###          Install zsh            ###
+###         Configure zsh           ###
 #######################################
-
-echo "### Install zsh ###"
-sudo dnf install -y zsh
 
 # Make zsh default
 sudo usermod --shell /bin/zsh asteurer
 
-
 #######################################
 # Install OhMyZsh
 #######################################
-
-echo "### Install OhMyZsh ###"
-
-# Install some helper tools
-sudo dnf install -y zsh-autosuggestions zsh-syntax-highlighting
 
 # Install OhMyZsh
 curl -o install-oh-my-zsh.sh -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
@@ -70,48 +72,21 @@ ln -sf dev-env/.zshrc ~/.zshrc
 ###         Install Docker          ###
 #######################################
 
-echo "### Install Docker ###"
-
-    # Remove old versions of Docker
-sudo dnf remove docker \
-                docker-client \
-                docker-client-latest \
-                docker-common \
-                docker-latest \
-                docker-latest-logrotate \
-                docker-logrotate \
-                docker-selinux \
-                docker-engine-selinux \
-                docker-engine
-
 sudo dnf -y install dnf-plugins-core
 sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl enable --now docker
 
 #######################################
-###        Install Golang           ###
-#######################################
-
-echo "### Install Golang ###"
-
-sudo dnf install -y golang
-
-#######################################
 ###         Install Rust            ###
 #######################################
-
-echo "### Install Rust ###"
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 #######################################
-###         Install tmux            ###
+###         Configure tmux          ###
 #######################################
 
-echo "### Install tmux ###"
-
-sudo dnf install -y tmux
 ln -sf dev-env/.tmux.conf ~/.tmux.conf
 
 # Download and install the plugins
@@ -119,30 +94,22 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ~/.tmux/plugins/tpm/bin/install_plugins all
 
 #######################################
-###         Install nvim            ###
+###         Configure nvim          ###
 #######################################
 
-echo "### Install nvim"
-sudo dnf install -y neovim
 sudo ln -sf /usr/bin/nvim /usr/bin/vi
-
-#######################################
-###      Install 1Password CLI      ###
-#######################################
-#ARCH="amd64"; \
-#OP_VERSION="v$(curl https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/N -s | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')"; \
-#    curl -sSfo op.zip \
-#    https://cache.agilebits.com/dist/1P/op2/pkg/"$OP_VERSION"/op_linux_"$ARCH"_"$OP_VERSION".zip \
-#    && unzip -od /usr/local/bin/ op.zip \
-#    && rm op.zip
-
-# You can use `op update`
-
-#######################################
-# Install Kubectl
-#######################################
 
 #######################################
 ###      Run CloudFlare Tunnel      ###
 #######################################
-# docker run cloudflare/cloudflared:latest tunnel --no-autoupdate run --token $CF_TOKEN
+
+# If this doesn't work the first time, try regenerating the credential, entering it into 1Password, and try again
+sudo docker run -d cloudflare/cloudflared:latest tunnel \
+    --no-autoupdate run \
+    --token $(op item get dev_env_cf_tunnel_token --fields label=credential --vault DEV --reveal)
+
+#######################################
+# Install Kubectl
+# Configure git signing and auth keys
+# Configure SSHing into a cloudflare tunnel instance
+#######################################
