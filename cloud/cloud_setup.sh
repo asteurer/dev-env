5d76cc476906852c7c72aeb527482ed3cabfbb46
Andrew Steurer <94206073+asteurer@users.noreply.github.com>
2025-07-27 19:57:57 -0500

    feat: spin workspace + cloud and local environment separation
    Signed-off-by: Andrew Steurer <94206073+asteurer@users.noreply.github.com>

---
+++ cloud/cloud_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#######################################
+###      Run shared scripts         ###
+#######################################
+
+./../shared/shared_setup.sh
+
+#######################################
+###    Addtl config for .zshrc      ###
+#######################################
+
+# Create a copy of the .zshrc.template file
+cp ../shared/.zshrc.template ../shared/.zshrc
---
+++ local/README.md
@@ -0,0 +1,3 @@
+# Overview
+
+To set up your development environment, simply run the `local_setup.sh` script on a Fedora server
